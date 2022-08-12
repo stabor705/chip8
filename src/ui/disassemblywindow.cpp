efa936d21cee4695cd986af87b267da722272792
Stanisław Borowy <stabor@startmail.com>
2022-08-12 12:48:07 +0200

    make disassembly window scroll automatically

--- src/ui/disassemblywindow.cpp
+++ src/ui/disassemblywindow.cpp
@@ -3,10 +3,9 @@
 
 #include <imgui.h>
 
-
 void DisassemblyWindow::load_program(const std::vector<uint8_t> &program) {
     Disassembler disassembler;
-    for (int i = 0; i < program.size() - 1; i++) {
+    for (int i = 0; i < program.size() - 1; i += 2) {
         uint16_t instr = program[i];
         instr = instr << 8;
         instr += program[i + 1];
@@ -18,7 +17,9 @@ void DisassemblyWindow::show() const {
     ImGui::Begin("Disassembly");
     for (int i = 0; i < disassembly.size(); i++) {
         ImGui::Text("%x: %s %s", starting_addr + i * 2, disassembly[i].c_str(),
-                    (i + starting_addr == pc) ? "<--" : "");
+                    (i * 2 + starting_addr == pc) ? "<--" : "");
+        if (i * 2 + starting_addr == pc)
+            ImGui::SetScrollHereY(0);
     }
     ImGui::End();
 }
